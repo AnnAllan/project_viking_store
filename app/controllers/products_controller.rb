551fde43187dae67260a84cf358abaf50c185e6a
Ann Allan <ann.cristina.allan@gmail.com>
2017-08-07 19:37:38 -0400

    Add pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @filter = params[:products_filter]
-    @products = @filter.present? ? Product.of_category(@filter) : Product.all
+    @products = @filter.present? ? Product.of_category(@filter).paginate(:page=> params[:page], :per_page => 6) : Product.paginate(:page=> params[:page], :per_page => 6)
   end
 
   def show
